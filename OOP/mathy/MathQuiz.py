6bb0fc9f89c5dc2df0bde9f6724de01fe1148116
Rebecca Bilbro <bilbro@gmail.com>
2015-09-19 15:09:50 -0400

    Adding OOP Workshop

---
+++ OOP/mathy/MathQuiz.py
@@ -0,0 +1,14 @@
+# MathQuiz.py
+
+from RatNum import RationalNumber
+
+print "Pop quiz! Find the sum, product, difference, and quotient for the following rational numbers:"
+a = RationalNumber(1,3)
+b = RationalNumber(2,3)
+
+print "The first number is: %s" %a
+print "The second number is: %s" %b
+print "The sum is: %s" %(a+b)
+print "The product is: %s" %(a*b)
+print "The difference is: %s" %(a-b)
+print "The quotient is: %s" %(a/b)
